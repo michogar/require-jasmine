55fe67f01af697ce365d0342384c100a210f12dd
Micho Garcia <michogar@gmail.com>
2013-10-07 13:11:20 +0200

    Testing with Jasmine & Require.js

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ require-jasmine
 ===============
 
 Use this to testing your app with Jasmine and Require.js
+
+
+* Clone or download this repo.
+* Move it to your project root folder.
+* Modify paths in SpecRunner.js to point your libraries.
+* Make tests and add them to the SpecRunner.js.
+* Run!!
+
+Fork and enjoy!
+
---
+++ spec/SpecRunner.js
@@ -0,0 +1,63 @@
+/**
+ * @author Micho García
+ */
+ // Modify as you need
+require.config({
+	baseUrl : "path/to_your/js_folder",
+	paths : {
+		jquery : 'libs/jquery',
+		underscore : 'libs/underscore',
+		backbone : 'libs/backbone',
+		jasmine : '../spec/lib/jasmine-1.2.0/jasmine',
+		'jasmine-html' : '../spec/lib/jasmine-1.2.0/jasmine-html',
+		spec : '../spec/'
+	},
+	shim : {
+		underscore : {
+			exports : "_"
+		},
+		backbone : {
+			deps : ['underscore', 'jquery'],
+			exports : 'Backbone'
+		},
+		'backbone.localStorage' : {
+			deps : ['backbone'],
+			exports : 'Backbone'
+		},
+		jasmine : {
+			exports : 'jasmine'
+		},
+		'jasmine-html' : {
+			deps : ['jasmine'],
+			exports : 'jasmine'
+		}
+	}
+});
+
+window.store = "TestStore";
+// override local storage store name - for testing
+
+require(['underscore', 'jquery', 'jasmine-html'], function(_, $, jasmine) {
+
+	var jasmineEnv = jasmine.getEnv();
+	jasmineEnv.updateInterval = 1000;
+
+	var htmlReporter = new jasmine.HtmlReporter();
+
+	jasmineEnv.addReporter(htmlReporter);
+
+	jasmineEnv.specFilter = function(spec) {
+		return htmlReporter.specFilter(spec);
+	};
+
+	var specs = [];
+
+	specs.push('path/to/whateverSpec');
+
+	$(function() {
+		require(specs, function() {
+			jasmineEnv.execute();
+		});
+	});
+
+});
